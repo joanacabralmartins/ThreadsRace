08fcf040edb87a0f224127b4dbc021d92f70a6fb
Joana Cabral <joanacabralmartins@outlook.com>
2023-08-21 19:29:06 -0300

    usando worker threads para buscar o numero

--- node-api/src/main.js
+++ node-api/src/main.js
@@ -1,3 +1,5 @@
+const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
+
 function buscarNumero(numero) {
   const iteracoes = 100000000;
   for (let i = 0; i < iteracoes; i++) {
@@ -8,4 +10,25 @@ function buscarNumero(numero) {
   return null;
 }
 
+if (isMainThread) {
+  const numThreads = 4;
+  const numero = 12345678; // numero que as threads vão buscar
+  const threads = [];
 
+  for (let i = 0; i < numThreads; i++) {
+    const worker = new Worker(__filename, {
+      workerData: { threadId: i },
+    });
+
+    threads.push(worker);
+  }
+} else { //WORKER
+  const { threadId } = workerData;
+
+  const result = buscarNumero(threadId);
+
+  // envia o resultado de volta para a thread principal
+  parentPort.postMessage(result);
+
+  process.exit();
+}

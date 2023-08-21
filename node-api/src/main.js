65a65767e0c911dfe5033aca9985ccb27849d7d1
Joana Cabral <joanacabralmartins@outlook.com>
2023-08-21 19:32:31 -0300

    calculo do tempo utilizado por cada worker thread

--- node-api/src/main.js
+++ node-api/src/main.js
@@ -14,6 +14,7 @@ if (isMainThread) {
   const numThreads = 4;
   const numero = 12345678; // numero que as threads vão buscar
   const threads = [];
+  const results = [];
 
   for (let i = 0; i < numThreads; i++) {
     const worker = new Worker(__filename, {
@@ -21,8 +22,15 @@ if (isMainThread) {
     });
 
     threads.push(worker);
+
+    const startTime = Date.now();
+    worker.on('message', (result) => {
+      const endTime = Date.now();
+      console.log(`Thread ${i} encontrou o número ${result} em ${endTime - startTime}ms.`);
+      results.push({ threadId: i, elapsedTime: endTime - startTime });
+    });
   }
-} else { //WORKER
+} else {
   const { threadId } = workerData;
 
   const result = buscarNumero(threadId);
